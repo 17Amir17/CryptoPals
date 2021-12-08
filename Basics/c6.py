9d505c37874a6047320de838b10e5f6704515ed7
17Amir17 <amirangel101@hotmail.com>
2021-12-08 22:04:58 +0200

    c6 prt1

--- Basics/c6.py
+++ Basics/c6.py
@@ -1,39 +1,24 @@
-def stringToBytes(str):
-    return str.encode('utf-8')
+def hammingDistance(x, y):
+    """
+    :type x: int
+    :type y: int
+    :rtype: int
+    """
+    ans = 0
+    for i in range(31,-1,-1):
+       b1= x>>i&1
+       b2 = y>>i&1
+       ans += not(b1==b2)
+    return ans
 
-def bytesToBits(b1):
-    bits = ''
-    for b in b1:
-        bts = bin(b)
-        for bt in bts:
-            bits += bt
-    return bits
-
-def getHammingDistance(s1, s2):
-    if(len(s1) != len(s2)):
+def hammingDiff(s1, s2):
+    s1Len = len(s1)
+    s2Len = len(s2)
+    if(s1Len != s2Len):
         return None
+    diff = 0
+    for i in range(0, s1Len):
+        diff += hammingDistance(ord(s1[i]), ord(s2[i]))
+    return diff
 
-    s1 = stringToBytes(s1)
-    s2 = stringToBytes(s2)
-    
-    bits1 = bytesToBits(s1)
-    bits2 = bytesToBits(s2)
-
-    bits = len(bits1)
-    bits2Len = len(bits2)
-
-    dist = 0
-    if bits < bits2Len:
-        dist = bits2Len - bits
-    else:
-        dist = bits - bits2Len
-        bits = bits2Len
-
-    for i in range(0, bits):
-        dist += int(bits1[i] != bits2[i])
-        
-    return dist
-
-print(getHammingDistance('this is a test', 'wokka wokka!!!'))
-
-
+print(hammingDiff('this is a test', 'wokka wokka!!!'))
